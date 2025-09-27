89ae5653364a5d5ed91f0e5845fbaa1f8edbbb69
AggelMast <aggelmast1@hotmail.com>
2025-09-27 19:25:46 +0300

    world build

--- main.lua
+++ main.lua
@@ -7,7 +7,11 @@ local map
 local scale = 2
 
 function love.load()
-	map = sti("tiled/map/1.lua")
+	map = sti("tiled/map/1.lua",{"box2d"})
+	world = love.physics.newWorld(0,0)
+	map:box2d_init(world)
+	map.layers.solid.visible = false
+	background = love.graphics.newImage()
 end
 
 function love.update(dt)
@@ -17,6 +21,7 @@ function love.update(dt)
 end
 
 function love.draw()
+	love.graphics.draw(background)
 	love.graphics.push()
 	if map and map.draw then
 		map:draw(0, 0, scale, scale)
