6e76db524b5b47270fc9194ee7a3b271b91033ff
dr/3301;/rev;8 <giagkospro@gmail.com>
2025-09-28 21:01:16 +0300

    Add anim8 library and custom font
    Introduced the anim8 animation library for LÖVE and added the OneTimeNbpRegular-YJyO.ttf font file. Updated conf.lua to set the new font as the default for the project.

--- conf.lua
+++ conf.lua
@@ -20,4 +20,6 @@ function love.conf(t)
 
   -- Modules: keep defaults (all on) for simplicity in a starter
   -- You can disable here later for perf/size if needed
+  -- font
+  t.font = love.graphics.newFont("D:/2d_paltaformer_lua_love2d_codevelopmet/OneTimeNbpRegular-YJyO.ttf")
 end
