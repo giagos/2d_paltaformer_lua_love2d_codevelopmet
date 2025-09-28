6bca8cb996ef3bd1c54a1d2ec35fc907aaa8df0d
AggelMast <aggelmast1@hotmail.com>
2025-09-28 03:46:44 +0300

    friction-jump tooning

--- player.lua
+++ player.lua
@@ -17,9 +17,13 @@ function Player:load(world, x, y)
    self.yVel = 0
    self.maxSpeed = 200
    self.acceleration = 4000
+<<<<<<< Updated upstream
    self.friction = 300
+=======
+   self.friction = 3400
+>>>>>>> Stashed changes
    self.gravity = 1500
-   self.jumpAmount = -500
+   self.jumpAmount = -350
    self.grounded = false
 
    self.color = {0.9, 0.3, 0.3, 1}
